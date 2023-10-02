f85a7f17ba23c82dc18a26322d5df405a6fbd65b
Gonlsali <radhita09@gmail.com>
2023-10-02 09:29:31 +0700

    WebDev Blade Tutorial

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view(
+    '/',
+    'index',
+    [
+        "pagetitle" => "Library",
+        "maintitle" => "Welcome to My Library"
+    ]
+);
+
+Route::view(
+    '/aboutus',
+    'about',
+    [
+        "pagetitle" => "About Us",
+        "maintitle" => "About My Library"
+    ]
+);
